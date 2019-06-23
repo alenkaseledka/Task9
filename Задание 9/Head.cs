00f9a6f1df26c7e02f6f9ba500a84b33ba67415e
Алёна Селивёрстова <avselivyorstova@edu.hse.ru>
2019-06-23 20:39:30 +0500

    Добавьте файлы проекта.

---
+++ Задание 9/Head.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_9
+{
+    class Head
+
+    {//класс для списка с информационным полем int
+
+        int info;//информационное поле
+
+        Head next;//адресное поле
+
+
+
+        //свойства
+
+
+
+        public int Info
+
+        {
+
+            get { return info; }
+
+            set { info = value; }
+
+        }
+
+        public Head Next
+
+        {
+
+            get { return next; }
+
+            set { next = value; }
+
+        }
+
+
+
+        //конструкторы
+
+
+
+        public Head()//конструктор без параметров
+
+        {
+
+            info = default(int);
+
+            next = null;
+
+        }
+
+
+
+        public override string ToString()
+
+        {
+
+            return info + " ";
+
+        }
+
+    }
+
+}
